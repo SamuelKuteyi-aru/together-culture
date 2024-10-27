c81cbe58733b4dc6c263a4d8da6378478c8fa01d
Ayo <Ayodejioragbade@gmail.com>
2024-10-27 23:19:45 +0000

    Added commit

--- together-culture-cambridge/Models/ErrorViewModel.cs
+++ together-culture-cambridge/Models/ErrorViewModel.cs
@@ -3,6 +3,6 @@ namespace together_culture_cambridge.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
+//check for request ID
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
