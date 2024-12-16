ad7bc0e9274b6189a89beb49a56e172b18d64bae
BYKiso <bk495@student.aru.ac.uk>
2024-12-16 00:06:13 +0000

    removed unused code

--- together-culture-cambridge/Controllers/SpaceBookingController.cs
+++ together-culture-cambridge/Controllers/SpaceBookingController.cs
@@ -162,60 +162,7 @@ namespace together_culture_cambridge.Controllers
             return Ok(spaceItem.Value);
         }
 
-        // GET: SpaceBooking/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var spaceBooking = await _context.SpaceBooking.FindAsync(id);
-            if (spaceBooking == null)
-            {
-                return NotFound();
-            }
-            ViewData["EndUserId"] = new SelectList(_context.EndUser, "Id", "Id", spaceBooking.EndUserId);
-            ViewData["SpaceId"] = new SelectList(_context.Space, "Id", "Id", spaceBooking.SpaceId);
-            return View(spaceBooking);
-        }
-
-        // POST: SpaceBooking/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,EndUserId,SpaceId,BookingDate")] SpaceBooking spaceBooking)
-        {
-            if (id != spaceBooking.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(spaceBooking);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SpaceBookingExists(spaceBooking.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["EndUserId"] = new SelectList(_context.EndUser, "Id", "Id", spaceBooking.EndUserId);
-            ViewData["SpaceId"] = new SelectList(_context.Space, "Id", "Id", spaceBooking.SpaceId);
-            return View(spaceBooking);
-        }
+       
 
         // GET: SpaceBooking/Delete/5
         public async Task<IActionResult> Delete(int? id)
